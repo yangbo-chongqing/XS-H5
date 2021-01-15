e01cfbc4334c57fd098e73e82b03787cff7b4f21
梁帮 <1535852346@qq.com>
2021-01-15 12:01:42 +0800

    词条富文本放大预览

--- src/views/page/editor/index.vue
+++ src/views/page/editor/index.vue
@@ -11,24 +11,40 @@
         <quill-editor ref="myTextEditor" v-model="content" :options="editorOption" style="min-height: 100px">
           <!-- 自定义toolar -->
           <div id="toolbar" slot="toolbar">
+            <div class="toolbar-left">
+              <div class="toolbar-withdraw">
+                <img src="@/assets/images/withdraw-show.png" alt=" ">
+              </div>
+              <div class="toolbar-nextstep">
+                <img src="@/assets/images/nextstep-show.png" alt=" ">
+              </div>
+              <div class="toolbar-edit">
+                <img src="@/assets/images/edit-show.png" alt=" ">
+              </div>
+            </div>
+            <div class="toolbar-right">
+              <button class="ql-image" title="照片"></button>
+              <button class="ql-video" title="视频"></button>
+              <button class="ql-clean" title="清除字体样式"></button>
+            </div>
+
             <!-- Add a bold button -->
-            <button class="ql-bold" title="加粗">Bold</button>
-            <button class="ql-italic" title="斜体">Italic</button>
-            <button class="ql-underline" title="下划线">underline</button>
-            <button class="ql-strike" title="删除线">strike</button>
-            <button class="ql-blockquote" title="引用"></button>
-            <button class="ql-code-block" title="代码"></button>
-            <button class="ql-header" value="1" title="标题1"></button>
-            <button class="ql-header" value="2" title="标题2"></button>
-            <!--Add list -->
-            <button class="ql-list" value="ordered" title="有序列表"></button>
-            <button class="ql-list" value="bullet" title="无序列表"></button>
-            <button class="ql-align" value="center" title="对齐"></button>
-            <button class="ql-align" value="right" title="对齐"></button>
+<!--            <button class="ql-bold" title="加粗">Bold</button>-->
+<!--            <button class="ql-italic" title="斜体">Italic</button>-->
+<!--            <button class="ql-underline" title="下划线">underline</button>-->
+<!--            <button class="ql-strike" title="删除线">strike</button>-->
+<!--            <button class="ql-blockquote" title="引用"></button>-->
+<!--            <button class="ql-code-block" title="代码"></button>-->
+<!--            <button class="ql-header" value="1" title="标题1"></button>-->
+<!--            <button class="ql-header" value="2" title="标题2"></button>-->
+<!--            &lt;!&ndash;Add list &ndash;&gt;-->
+<!--            <button class="ql-list" value="ordered" title="有序列表"></button>-->
+<!--            <button class="ql-list" value="bullet" title="无序列表"></button>-->
+<!--            <button class="ql-align" value="center" title="对齐"></button>-->
+<!--            <button class="ql-align" value="right" title="对齐"></button>-->
 <!--            <button class="ql-align" value="align" title="对齐"></button>-->
-            <button class="ql-image" title="照片"></button>
-            <button class="ql-video" title="视频"></button>
-            <button class="ql-clean" title="清除字体样式"></button>
+
+
           </div>
         </quill-editor>
       </div>
